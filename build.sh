cc01d6e7e126a0f32c6909d1085b54ca522eeff2
RJ Bergeron <rbergero@gmail.com>
2018-04-24 01:25:39 -0400

    okay let's try this

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -eux
+set -o pipefail
+
+GPG_PASSFILE=(/dev/shm/pass.*)
+
+printf 'DEBSIGN_PROGRAM="gpg --no-use-agent --no-tty --trusted-key 0x7D1110294E694719 --passphrase-file %s"\nDEBSIGN_KEYID=%s\n' "${GPG_PASSFILE[0]}" "0x7D1110294E694719" > "${HOME}/.devscripts"
+
+srcdir=$(pwd)
+
+backportpackage -s artful -d xenial -u ppa:test/ppa libssh2
